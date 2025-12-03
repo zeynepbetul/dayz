c954bc550f001f7c99c20d6599ba6ad0e0408fb9
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-03 13:43:42 +0300

    add func configure for custom button

--- Dayz/DZButton.swift
+++ Dayz/DZButton.swift
@@ -11,9 +11,17 @@ class DZButton: UIButton {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius       = 10
+        titleLabel?.textColor    = .white
+        titleLabel?.font         = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
     }
 }
