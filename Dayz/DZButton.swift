1df5bb72012a7b50679bbe566317f273bf018872
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-03 12:49:41 +0300

    add new custom button class

---
+++ Dayz/DZButton.swift
@@ -0,0 +1,19 @@
+//
+//  DZButton.swift
+//  Dayz
+//
+//  Created by Zeynep Betül Kaya on 3.12.2025.
+//
+
+import UIKit
+
+class DZButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
