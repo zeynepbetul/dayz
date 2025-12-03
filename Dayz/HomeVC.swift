d0dbb1969530e659ea73bdc167b99cda6496f684
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-03 11:48:57 +0300

    add BarButtonItems to home navigationcontroller

--- Dayz/HomeVC.swift
+++ Dayz/HomeVC.swift
@@ -8,9 +8,20 @@
 import UIKit
 
 class HomeVC: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
+        
+        navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .search, target: self, action: #selector(searchTapped))
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "bell"), style: .plain, target: self, action: #selector(notificationsTapped))
+    }
+    
+    @objc func searchTapped() {
+        
+    }
+    
+    @objc func notificationsTapped() {
+        
     }
 }
