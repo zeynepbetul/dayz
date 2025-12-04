323ab7005b6f9767f9455facc329d9ff7ba401c6
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-04 15:24:58 +0300

    1. add dummy data to create fetched data  2. tableView.register

---
+++ Dayz/SearchCell.swift
@@ -0,0 +1,57 @@
+//
+//  SearchCell.swift
+//  Dayz
+//
+//  Created by Zeynep Betül Kaya on 3.12.2025.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+    
+    var avatarImageView = DZAvatarImageView(frame: .zero)
+    var userNameLabel   = UILabel()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview(avatarImageView)
+        addSubview(userNameLabel)
+        
+        configureTitleLabel()
+        setImageConstraints()
+        setUserNameLabelConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func set(dzuser: DZUser) {
+        avatarImageView = dzuser.avatarImage
+        userNameLabel.text = dzuser.userName
+    }
+    
+    func configureTitleLabel() {
+        userNameLabel.numberOfLines = 0 // it is not going to pass new line
+        userNameLabel.adjustsFontSizeToFitWidth = true
+    }
+    
+    func setImageConstraints() {
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: centerYAnchor),  // y-axis is equal to the center y-axis of table view cell
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 12),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 40),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+    
+    func setUserNameLabelConstraints() {
+        userNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            userNameLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
+            userNameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 10),
+            userNameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -12),
+            userNameLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
+}
