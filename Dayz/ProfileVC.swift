3c1dda5e3b538c1a1bd84cdb820fba9fdb43ab1e
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-06 22:44:45 +0300

    use DZAlert on Profile to sign out

--- Dayz/ProfileVC.swift
+++ Dayz/ProfileVC.swift
@@ -12,5 +12,27 @@ class ProfileVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBrown
+        configureLogOutButton()
+    }
+    
+    func configureLogOutButton() {
+        let button = UIButton()
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(logout), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Log Out", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        
+        NSLayoutConstraint.activate([
+            button.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.widthAnchor.constraint(equalToConstant: 150),
+            button.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+    
+    @objc func logout() {
+        presentDZAlertOnMainThread(title: "Sign out", message: "Do you want to sign out of your account?",
+                                   buttonTitle: "Sign Out", buttonTitleSec: "Cancel")
     }
 }
