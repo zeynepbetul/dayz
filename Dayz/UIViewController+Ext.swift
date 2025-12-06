87c150b196f128bd408c9609fc7b28ed0d7a9e5e
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-06 22:43:42 +0300

    add UIViewController extension for DZAlertVC

--- Dayz/UIViewController+Ext.swift
+++ Dayz/UIViewController+Ext.swift
@@ -5,4 +5,16 @@
 //  Created by Zeynep Betül Kaya on 6.12.2025.
 //
 
-import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func presentDZAlertOnMainThread(title: String, message: String, buttonTitle: String, buttonTitleSec: String?) {
+        DispatchQueue.main.async {
+            let alertVC = DZAlertVC(title: title, message: message, buttonTitle: buttonTitle, buttonTitleSec: buttonTitleSec)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
