d0b644d5afef9dcd8a81081edd531ad9603228fc
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-09 15:23:02 +0300

    give left padding to textfield texts

--- Dayz/Custom Views/DZTextField.swift
+++ Dayz/Custom Views/DZTextField.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class DZTextField: UITextField {
+    
+    private let padding: CGFloat = 12
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -28,17 +30,27 @@ class DZTextField: UITextField {
         translatesAutoresizingMaskIntoConstraints = false
         
         layer.cornerRadius          = 10
-        layer.borderWidth           = 2
+        layer.borderWidth           = 1
         layer.borderColor           = UIColor.systemGray4.cgColor
         
         textColor                   = .label
-        tintColor                   = .label // when textfield is selected, what will be the text color
+        tintColor                   = .label
         textAlignment               = .left
-        font                        = UIFont.preferredFont(forTextStyle: .title2)
+        font                        = UIFont.preferredFont(forTextStyle: .body)
         adjustsFontSizeToFitWidth   = true // font size will automatically be smaller when long text is entered
         minimumFontSize             = 12 // but min will be 12
         
         backgroundColor             = .tertiarySystemBackground
         autocorrectionType          = .no // no autocorrection for entered text
     }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+        }
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+        }
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: padding, dy: 0)
+        }
 }
