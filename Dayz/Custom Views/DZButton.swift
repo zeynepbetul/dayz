6c22fa3702c955870207b53a5bc04a3981bc24fd
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-09 15:26:22 +0300

    change corner radius of the DZButton

--- Dayz/Custom Views/DZButton.swift
+++ Dayz/Custom Views/DZButton.swift
@@ -26,7 +26,7 @@ class DZButton: UIButton {
     }
     
     private func configure() {
-        layer.cornerRadius       = 10
+        layer.cornerRadius       = 25
         titleLabel?.textColor    = .white
         titleLabel?.font         = UIFont.preferredFont(forTextStyle: .headline)
         translatesAutoresizingMaskIntoConstraints = false
