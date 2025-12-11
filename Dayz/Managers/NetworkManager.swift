8207071f876c8c782e639d5fdee0390b8fe47118
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-11 14:44:46 +0300

    add NetworkManager

---
+++ Dayz/Managers/NetworkManager.swift
@@ -0,0 +1,16 @@
+//
+//  NetworkManager.swift
+//  Dayz
+//
+//  Created by Zeynep Betül Kaya on 11.12.2025.
+//
+
+import Foundation
+// Singleton Network Manager
+class NetworkManager {
+    let shared = NetworkManager()
+    
+    private init() {
+        
+    }
+}
