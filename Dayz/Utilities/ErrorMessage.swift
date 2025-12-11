f079370b23a61bcf2d1efbc61c5b73461524f64e
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-12 00:46:29 +0300

    add new folder utilities and file errorMessage

---
+++ Dayz/Utilities/ErrorMessage.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorMessage.swift
+//  Dayz
+//
+//  Created by Zeynep Betül Kaya on 12.12.2025.
+//
+
+import Foundation
+
+enum ErrorMessage: String {
+    case usernameTaken = "This username is already taken."
+    case networkError = "A network error occurred."
+}
