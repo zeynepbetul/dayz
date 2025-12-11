3aad5f096ffa3e55ba37cc68dba2462377588dec
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-11 17:42:27 +0300

    add public and private user models

--- Dayz/Model/User.swift
+++ Dayz/Model/User.swift
@@ -8,14 +8,29 @@
 import Foundation
 
 struct User: Codable {
-    var login: String
-    var avatarUrl: String
+    var id: String
+    var username: String
+    var email: String
     var name: String?
     var bio: String?
-    var publicCalendar: Bool
-    var sharedCalendar: Bool
-    var privateCalendar: Bool
-    var following: Int
+    var avatarUrl: String?
     var followers: Int
-    var createdAt: String
+    var following: Int
+    var createdAt: Date
+}
+
+struct PublicUser: Codable {
+    var id: String
+    var username: String
+    var name: String?
+    var bio: String?
+    var avatarUrl: String?
+    var followers: Int
+    var following: Int
+}
+
+struct PrivateUser: Codable {
+    var id: String
+    var email: String
+    var createdAt: Date
 }
