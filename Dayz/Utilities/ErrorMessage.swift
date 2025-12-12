9a01cc8982d5782509ebe79593a02b7a01ca3af6
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-12 17:14:25 +0300

    change errorMessage file to use Swift’s Result type for type-safe and consistent error handling.

--- Dayz/Utilities/ErrorMessage.swift
+++ Dayz/Utilities/ErrorMessage.swift
@@ -7,10 +7,11 @@
 
 import Foundation
 
-enum ErrorMessage: String {
+enum DZError: String, Error {
     case usernameTaken = "This username is already taken."
     case networkError = "A network error occurred."
     case emailVerificationError = "Error in sending verification email."
     case unknownError = "An unknown error occured."
     case userCreateError = "Error occured in creating user."
+    case loadUsersError = "Error in loading users"
 }
