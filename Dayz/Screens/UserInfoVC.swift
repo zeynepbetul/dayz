4779dcc242ae81aaaee47faae72987bf3f1553df
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-16 17:38:37 +0300

    add UserInfoVC

---
+++ Dayz/Screens/UserInfoVC.swift
@@ -0,0 +1,16 @@
+//
+//  UserInfoVC.swift
+//  Dayz
+//
+//  Created by Zeynep Betül Kaya on 16.12.2025.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
