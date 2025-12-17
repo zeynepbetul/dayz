62fbe40316c33df0906d247f2ab8f52a0d6e0a7b
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-17 20:50:43 +0300

    implement DZUserInfoHeaderVC for userInfoVC

---
+++ Dayz/Custom Views/ViewControllers/DZUserInfoHeaderVC.swift
@@ -0,0 +1,95 @@
+//
+//  DZUserInfoHeaderVCViewController.swift
+//  Dayz
+//
+//  Created by Zeynep Betül Kaya on 17.12.2025.
+//
+
+import UIKit
+
+class DZUserInfoHeaderVC: UIViewController {
+    
+    let avatarImageView       = DZAvatarImageView(frame: .zero)
+    let usernameLabel         = DZTitleLabel(textAlignment: .left, fontSize: 34)
+    let nameLabel             = DZSecondaryTitleLabel(textAlignment: .center, fontSize: 18)
+    let followersLabel        = DZSecondaryTitleLabel(textAlignment: .center, fontSize: 18)
+    let followingLabel        = DZSecondaryTitleLabel(textAlignment: .center, fontSize: 18)
+    let followButton          = DZButton(backgroundColor: .black, title: "Follow")
+    let bioLabel              = DZBodyLabel(textAlignment: .left)
+
+    var user: PublicUser!
+    
+    init(user: PublicUser!) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addSubviews()
+        layoutUI()
+        configureUIElements()
+    }
+    
+    func configureUIElements() {
+        avatarImageView.setImage(from: user.avatarUrl)
+        usernameLabel.text       = user.username
+        nameLabel.text           = user.name ?? ""
+        followersLabel.text      = "Followers: \(user.followers)"
+        followingLabel.text      = "Follow: \(user.following)"
+        bioLabel.text            = user.bio ?? ""
+        bioLabel.numberOfLines   = 3
+    }
+    
+    func addSubviews() {
+        view.addSubview(avatarImageView)
+        view.addSubview(usernameLabel)
+        view.addSubview(nameLabel)
+        view.addSubview(followersLabel)
+        view.addSubview(followingLabel)
+        view.addSubview(followButton)
+        view.addSubview(bioLabel)
+    }
+    
+    func layoutUI() {
+        let padding: CGFloat            = 20
+        let textImagePadding: CGFloat   = 12
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 90),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 90),
+            
+            nameLabel.topAnchor.constraint(equalTo: avatarImageView.topAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: textImagePadding),
+            nameLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            nameLabel.heightAnchor.constraint(equalToConstant: 38),
+            
+            followersLabel.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor, constant: 8),
+            followersLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: textImagePadding),
+            followersLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            followersLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            followingLabel.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor),
+            followingLabel.leadingAnchor.constraint(equalTo: followersLabel.trailingAnchor, constant: 5),
+            followingLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            followingLabel.heightAnchor.constraint(equalToConstant: 20),
+            
+            followButton.bottomAnchor.constraint(equalTo: avatarImageView.bottomAnchor),
+            followButton.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: textImagePadding),
+            followButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            followButton.heightAnchor.constraint(equalToConstant: 44),
+            
+            bioLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: textImagePadding),
+            bioLabel.leadingAnchor.constraint(equalTo: avatarImageView.leadingAnchor),
+            bioLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            bioLabel.heightAnchor.constraint(equalToConstant: 60)
+
+        ])
+    }
+}
