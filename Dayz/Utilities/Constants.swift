fa3ab1901335802d89f0de5bcb879397ac411538
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-17 20:52:25 +0300

    add new file: Constants

---
+++ Dayz/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Dayz
+//
+//  Created by Zeynep Betül Kaya on 17.12.2025.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
