41489014bd9e0ba8661faf4276a2a839c04efa6c
zeynepbetul <z.beetulkaya@gmail.com>
2025-12-21 18:48:19 +0300

    update with new functions in PersistenceManager

--- Dayz/Managers/PersistenceManager.swift
+++ Dayz/Managers/PersistenceManager.swift
@@ -7,12 +7,41 @@
 
 import Foundation
 
+enum PersistenceActionType {
+    case add, remove
+}
+
 enum PersistenceManager {
     
     static private let defaults = UserDefaults.standard
     
     enum Keys {
         static let recentlyVisitedProfiles = "recents"
+    }
+    
+    static func updateWith(favorite: PublicUser, actionType: PersistenceActionType, completed: @escaping (DZError?) -> Void) {
+        retrieveRecentlyVisitedProfiles { result in
+            switch result {
+            case .success(let favorites):
+                var retrievedFavorites = favorites
+                
+                switch actionType {
+                case .add:
+                    guard !retrievedFavorites.contains(favorite) else {
+                        // completed(.alreadyInFavorites)
+                        return
+                    }
+                    retrievedFavorites.append(favorite)
+                case .remove:
+                    retrievedFavorites.removeAll { $0.username == favorite.username }
+                }
+                
+                completed(save(favorites: favorites))
+                
+            case .failure(let error):
+                completed(error)
+            }
+        }
     }
     
     static func retrieveRecentlyVisitedProfiles(completed: @escaping (Result<[PublicUser], DZError>) -> Void) {
@@ -27,6 +56,17 @@ enum PersistenceManager {
             completed(.success(profiles))
         } catch {
             completed(.failure(.unableToFavorite))
+        }
+    }
+    
+    static func save(favorites: [PublicUser]) ->DZError? {
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(favorites)
+            defaults.set(encodedFavorites, forKey: Keys.recentlyVisitedProfiles)
+            return nil
+        } catch {
+            return .unableToFavorite
         }
     }
 }
